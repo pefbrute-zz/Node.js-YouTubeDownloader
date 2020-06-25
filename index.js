666555d44b5e7c45eac5df8d3deca3107c55b4dd
pefbrute <proggraming@mail.ru>
2020-06-25 18:14:46 +0300

    +finished-project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
 const app = express();
+
 app.use('/static', express.static('./static'));
 
 app.listen(3000, () => { 
